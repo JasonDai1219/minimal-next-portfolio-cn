0fcbc739af41f9088c45b6c05e9eb07ea000bcfd
Jason(Ruizhe) Dai <114436475+JasonDai1219@users.noreply.github.com>
2026-02-25 22:31:03 -0500

    fix: add explicit types to pagesInfoArr map

---
+++ config/contributions.ts
@@ -0,0 +1,10 @@
+export interface contributionsInterface {
+  title: string;
+  description?: string;
+  href?: string;
+  icon?: string;
+  date?: string;
+  tags?: string[];
+}
+
+export const contributions: contributionsInterface[] = [];
